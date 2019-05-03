570cc968b05689f1a86f1e6b9c977b84f52bd54d
Shramee Srivastav <shramee.srivastav@gmail.com>
2019-05-03 19:02:12 +0800

    Fixing search
    and refactoring login/out redirect funcs.

--- inc/traveler_mods.php
+++ inc/traveler_mods.php
@@ -121,20 +121,18 @@ if(!function_exists( 'st_vc_list_tour' )) {
 
 }
 
-if ( ! function_exists( 'st_after_logout_redirect' ) ) {
-	function st_after_logout_redirect( $redirect_to, $requested_redirect_to, $user ) {
-		return st_after_login_redirect( $redirect_to, $requested_redirect_to, $user );
-	}
-}
-
 if ( ! function_exists( 'st_after_login_redirect' ) ) {
 	function st_after_login_redirect( $redirect_to, $request, $user ) {
 		if ( $_SERVER['HTTP_REFERER'] ) {
 			$redirect_to = $_SERVER['HTTP_REFERER'];
 		}
 
-		die( $redirect_to );
-
 		return $redirect_to;
+	}
+}
+
+if ( ! function_exists( 'st_after_logout_redirect' ) ) {
+	function st_after_logout_redirect( $redirect_to, $requested_redirect_to, $user ) {
+		return st_after_login_redirect( $redirect_to, $requested_redirect_to, $user );
 	}
 }
--- searchform.php
+++ searchform.php
@@ -12,6 +12,9 @@
 ?>
 <form role="search" method="get" class="search" action="<?php echo home_url( '/' ); ?>">
 	<input type="text" class="form-control" value="<?php echo get_search_query() ?>" name="s" placeholder="<?php st_the_language( 'search...' ) ?>">
-	<input type="hidden" name="post_type" value="post">
+	<input type="hidden" name="post_type[]" value="post">
+	<input type="hidden" name="post_type[]" value="location">
+	<input type="hidden" name="post_type[]" value="st_hotel">
+	<input type="hidden" name="post_type[]" value="st_rental">
 	<button class="search-button"><?php st_the_language( 'Search' ) ?></button>
 </form>
